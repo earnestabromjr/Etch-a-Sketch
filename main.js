69598aea3442b28a01cb67d15c0c9ba0d2b7cc1d
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-11-09 22:11:50 -0600

    Added setup function which creates div grid \n Created changeColors function which changes colors of Node list items

--- main.js
+++ main.js
@@ -4,9 +4,23 @@ const inputColumns = 16;
 const inputRows = 16;
 
 // Loop to create varied number of divs
-for (let i = 0; i < inputRows * inputColumns; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.classList.add('grid-item')
-    container.appendChild(gridDiv);
+function setup(inputColumns, inputRows) {
+    for (let i = 0; i < inputRows * inputColumns; i++) {
+        let gridDiv = document.createElement('div');
+        gridDiv.classList.add('grid-item')
+        container.appendChild(gridDiv);
+    }
 }
 
+function changeColors(element) {
+    element.style.backgroundColor = "darkblue";
+}
+
+setup(inputColumns, inputRows);
+const buttonContainer = document.getElementById('buttons-container');
+const divNumber = document.getElementById('divNumber');
+
+const newGrid = container.childNodes;
+newGrid.addEventListener('mouseleave', (e) => {
+    newGrid.onmousemove =
+})
