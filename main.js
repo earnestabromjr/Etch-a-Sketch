129a17656907f1728410ede7fb9d31f1ca90e3fd
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-11-11 20:51:46 -0600

    Completed changeColors function and changed logic of setup function

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 const container = document.getElementById('container');
 
-const inputColumns = 16;
-const inputRows = 16;
+let inputColumns = 16;
+let inputRows = 16;
 
 // Loop to create varied number of divs
 function setup(inputColumns, inputRows) {
     for (let i = 0; i < inputRows * inputColumns; i++) {
         let gridDiv = document.createElement('div');
-        gridDiv.classList.add('grid-item')
+        gridDiv.classList.add('grid-item');
+        gridDiv.addEventListener('mouseover', changeColors);
+
         container.appendChild(gridDiv);
     }
 }
 
 function changeColors(element) {
-    element.style.backgroundColor = "darkblue";
+    element.target.style.backgroundColor = "#36013f";
+}
+
+function resetGrid() {
+    const cells = document.getElementsByClassName('grid-item');
+    Array.from(cells).forEach(cell => {
+        cell.style.backgroundColor = 'white';
+    });
+}
+
+function changeGrid() {
+    inputColumns = parseInt(prompt("Enter Column number: "));
+    inputRows = prompt(("Enter Row number: "));
+    setup(inputColumns = 16, inputRows = 16);
 }
 
 setup(inputColumns, inputRows);
 const buttonContainer = document.getElementById('buttons-container');
 const divNumber = document.getElementById('divNumber');
 
-const newGrid = container.childNodes;
-newGrid.addEventListener('mouse', (e) => {
-    changeColors(e.target);
-})
+
