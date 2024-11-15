a7d0c7fc61a40e9b3772dc84e109c10e7a2e7c31
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-11-15 16:26:12 -0600

    Moved logic to create cells from setup function to createCells function

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const container = document.getElementById('container');
-
-let inputColumns = 16;
-let inputRows = 16;
-
 // Loop to create varied number of divs
 function setup(inputColumns, inputRows) {
+    const container = document.getElementById('container');
+    container.id = 'container';
+    createCells(16, 16, container);
+}
+
+function createCells(inputColumns, inputRows, container) {
     for (let i = 0; i < inputRows * inputColumns; i++) {
         let gridDiv = document.createElement('div');
         gridDiv.classList.add('grid-item');
+        gridDiv.style.height = inputRows + 'px';
+        gridDiv.style.width = inputColumns + 'px';
+        container.style.width = (inputColumns * inputRows) +'px';
         gridDiv.addEventListener('mouseover', changeColors);
 
         container.appendChild(gridDiv);
@@ -15,28 +19,37 @@ function setup(inputColumns, inputRows) {
 }
 
 function changeColors(element) {
-    element.target.style.backgroundColor = "#36013f";
+    element.target.style.backgroundColor = "darkgreen";
 }
 
 function resetGrid() {
     const cells = document.getElementsByClassName('grid-item');
     Array.from(cells).forEach(cell => {
-        cell.style.backgroundColor = 'white';
+        cell.style.backgroundColor =  '#4CAF50';
     });
 }
 
 function changeGrid() {
-    inputColumns = parseInt(prompt("Enter Column number: "));
-    inputRows = prompt(("Enter Row number: "));
-    const cells = document.getElementsByClassName('grid-item');
-    Array.from(cells).forEach(cell => {
-        cell.style.width = inputColumns + 'px';
-        cell.style.height = inputRows + 'px';
+    let inputColumns = parseInt(prompt("Enter Column number: "));
+    let inputRows = prompt(("Enter Row number: "));
+    const container = document.getElementById('container');
+    let cells = container.childNodes;
+    cells.forEach(cell => {
+        cell.remove();
     })
-    setup(inputColumns = 16, inputRows = 16);
+   createCells(inputColumns, inputRows, container);
+    let gridDiv = document.getElementById('grid-item');
+    gridDiv.style.width = '16px';
+    gridDiv.style.height = '16px';
+    container.style.width = inputColumns * inputRows + 'px';
+    // const cells = document.getElementsByClassName('grid-item');
+    // Array.from(cells).forEach(cell => {
+    //     cell.style.width = inputColumns + 'px';
+    //     cell.style.height = inputRows + 'px';
+    // })
 }
 
-setup(inputColumns, inputRows);
+setup(16, 16);
 const buttonContainer = document.getElementById('buttons-container');
 const divNumber = document.getElementById('divNumber');
 
