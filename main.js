e44b2cae8305c1ad60fb0adc9b235d74f3f2068d
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-11-16 08:02:29 -0600

    Added random color function Need to implement logic to choose from random color or a specific color

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 // Loop to create varied number of divs
-function setup(inputColumns, inputRows) {
+function setup(gridNumber) {
     const container = document.getElementById('container');
     container.id = 'container';
-    createCells(16, 16, container);
+    createCells(gridNumber, container);
 }
 
-function createCells(inputColumns, inputRows, container) {
-    for (let i = 0; i < inputRows * inputColumns; i++) {
+function createCells(gridNumber, container) {
+    for (let i = 0; i < gridNumber ** 2; i++) {
         let gridDiv = document.createElement('div');
         gridDiv.classList.add('grid-item');
-        gridDiv.style.height = (256 / inputColumns) + 'px';
+        gridDiv.style.height = (256 / gridNumber) + 'px';
         gridDiv.style.width = 'auto';
-        // container.style.width = (inputColumns * inputRows) +'px';
         gridDiv.addEventListener('mouseover', changeColors);
 
         container.appendChild(gridDiv);
     }
 }
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 function changeColors(element) {
-    element.target.style.backgroundColor = "darkgreen";
+    element.target.style.backgroundColor = getRandomColor();
 }
 
 function resetGrid() {
@@ -30,27 +38,21 @@ function resetGrid() {
 }
 
 function changeGrid() {
-    let inputColumns = parseInt(prompt("Enter Column number: "));
-    let inputRows = prompt(("Enter Row number: "));
+    let gridNumber = parseInt(prompt("Enter number of squares per side: "));
+    if (gridNumber > 99) {
+        alert("Number must be less than 100")
+        return;
+    }
     const container = document.getElementById('container');
-    let cells = container.childNodes;
-    cells.forEach(cell => {
-        cell.remove();
-    })
+
     container.innerHTML = '';
-   createCells(inputColumns, inputRows, container);
+    createCells(gridNumber, container);
     let gridDiv = document.getElementById('grid-item');
     gridDiv.style.width = 'auto';
     gridDiv.style.height = 'auto';
-    // container.style.width = inputColumns * inputRows + 'px';
-    // const cells = document.getElementsByClassName('grid-item');
-    // Array.from(cells).forEach(cell => {
-    //     cell.style.width = inputColumns + 'px';
-    //     cell.style.height = inputRows + 'px';
-    // })
 }
 
-setup(16, 16);
+setup(16);
 const buttonContainer = document.getElementById('buttons-container');
 const divNumber = document.getElementById('divNumber');
 
